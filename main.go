4485d7c3503c1c7660cb2f1ee996720c90ad3aa3
Siddharth Kannan <kannan.siddharth12@gmail.com>
2018-02-17 16:57:48 +0530

    write the fetched subjects to a json file

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
     "log"
     "github.com/joho/godotenv"
+    "io/ioutil"
+    "encoding/json"
+    "time"
+    "os"
+    "fmt"
 )
 func main() {
 	err := godotenv.Load()
@@ -36,6 +41,18 @@ func main() {
 
     for i := 0; i < len(departments); i++ {
         some_val := <-accumulate_channel
-        log.Printf("Fetch completed for department %s with %d subjects", some_val.Department, len(some_val.Subjects))
+        log.Printf("Fetch completed for department %s with %d subjects",
+                            some_val.Department,
+                            len(some_val.Subjects))
+
+        b, err := json.Marshal(some_val.Subjects)
+        if err != nil {
+            b = []byte("")
+        }
+
+        err = ioutil.WriteFile(fmt.Sprintf("%s.json", some_val.Department), b, 0644)
+        if err != nil {
+            log.Printf("Could not write %s.json to file: %v", some_val.Department, err)
+        }
     }
 }
