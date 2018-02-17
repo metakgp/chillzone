2add8e54e4898ae7595aa96f3f0b08a7afc41663
Siddharth Kannan <kannan.siddharth12@gmail.com>
2018-02-17 21:57:20 +0530

    incorporate bootstrap table with colored cells

--- chill/src/GenerateCustomTR.js
+++ chill/src/GenerateCustomTR.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Alert } from 'react-bootstrap'
 
 class GenerateCustomTR extends Component {
   static PropTypes = {
     schedule: PropTypes.object
   }
 
+  getTextToDisplay(k) {
+    return k === 0 ? "EMPTY" : "TAKEN";
+  }
+
+  getAlertLevel(k) {
+    return k === 0 ? "success" : "danger";
+  }
+
   render() {
     return (
       <tr>
         {this.props.schedule.map(k => {
-          return <td>{k === 0 ? "EMPTY" : "TAKEN"}</td>
+          return (
+            <td>
+            <Alert bsStyle={this.getAlertLevel(k)}>
+              {this.getTextToDisplay(k)}
+            </Alert>
+          </td>
+          )
         })}
       </tr>
     )
