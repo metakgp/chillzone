b9e648f91b766ac494bae53a6630171e41b88bc6
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2018-04-15 14:59:02 +0530

    Refactor code related to Room data

--- chillz/rooms.go
+++ chillz/rooms.go
@@ -1,60 +1,37 @@
 package chillz
 
-var rooms = []string{
-	"NR121",
-	"NR122",
-	"NR123",
-	"NR124",
-	"NR221",
-	"NR222",
-	"NR223",
-	"NR224",
-	"NR321",
-	"NR322",
-	"NR323",
-	"NR324",
-	"NR421",
-	"NR422",
-	"NR423",
-	"NR424",
-	"NC141",
-	"NC142",
-	"NC131",
-	"NC132",
-	"NC241",
-	"NC242",
-	"NC243",
-	"NC244",
-	"NC231",
-	"NC232",
-	"NC233",
-	"NC234",
-	"NC341",
-	"NC342",
-	"NC343",
-	"NC344",
-	"NC331",
-	"NC332",
-	"NC333",
-	"NC334",
-	"NC441",
-	"NC442",
-	"NC443",
-	"NC444",
-	"NC431",
-	"NC432",
-	"NC433",
-	"NC434",
-	"V1",
-	"V2",
-	"V3",
-	"V4",
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Room struct {
+	Room     string `json:"room"`
+	Location string `json:"location"`
 }
 
-func get_init_room_map() map[string][5][9]int {
+type Rooms []Room
+
+// Fetches all the rooms from a JSON file, primarily rooms.json
+//
+func GetRooms(filename string) (Rooms, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return Rooms{}, err
+	}
+
+	var rooms Rooms
+	err = json.Unmarshal(data, &rooms)
+	if err != nil {
+		return Rooms{}, err
+	}
+	return rooms, nil
+}
+
+func get_init_room_map(rooms Rooms) map[string][5][9]int {
 	ret := make(map[string][5][9]int)
 	for _, k := range rooms {
-		ret[k] = [5][9]int{}
+		ret[k.Room] = [5][9]int{}
 	}
 	return ret
 }
