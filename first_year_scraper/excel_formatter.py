0419b8da5e6df206c93ab5af016d313003150f63
iakshat <akshat1118@gmail.com>
2020-09-19 10:34:06 +0530

    removing comments further

--- first_year_scraper/excel_formatter.py
+++ first_year_scraper/excel_formatter.py
@@ -14,20 +14,6 @@ def sanitise_sheets(valid_sheets, wb):
         ws = wb.get_sheet_by_name(sheet)
         ws.delete_cols(1,2)
         ws.delete_rows(1,2)
-        # merged_ranges = ws.merged_cell_ranges # get all merged ranges in sheet #TAKEN CARE OF BY PARSER
-        # for merged_range in merged_ranges:
-        #     boundary = merged_range.bounds
-        #     original_value = ws[getkey(boundary[0], boundary[1])].value
-        #     ws.unmerge_cells(str(merged_range))
-        #     for i in range(boundary[0], boundary[2]+1):
-        #         for j in range(boundary[1], boundary[3]+1):
-        #             ws[getkey(i,j)].value = original_value
-    # for sheet in valid_sheets:
-    #     ws = wb.get_sheet_by_name(sheet)
-    #     depth = 10
-    #     while(ws['A1'].value != "Period" and depth):
-    #         ws.delete_rows(1)
-    #         depth-=1;
 
 
 def format_cell(original_value):
@@ -65,12 +51,11 @@ def map_sheets(valid_sheets, wb):
     slot_fillup = set()
 
     for sheet in valid_sheets:
-        # print("formattting ", sheet)
         sub_vs_room = {}
         sub_vs_slot = {}
         ws = wb.get_sheet_by_name(sheet)
         if(ws['L2'].value is not None or ws['K2'].value is None):
-            print("parsing error in sheet {}, please include it in first_year.csv manually".format(sheet))
+            print("parsing error in sheet {}, please inclue it in first_year.csv manually".format(sheet))
             continue
 
         for i in range(0, 6):
@@ -84,16 +69,8 @@ def map_sheets(valid_sheets, wb):
                     sub_vs_room[subject] = cell_value[1:]
                     if(subject not in sub_vs_slot):
                         sub_vs_slot[subject] = list()
-                    # print(subject, i, j)
                     sub_vs_slot[subject].append(str(i)+str(j-1 if j>5 else j))
 
-                    # for k in range(1, len(cell_value)):
-                    #     if(str(i)+str(j-1 if j>5 else j) in slot.slot_map):
-                    #         mapped_slot = slot.slot_map[str(i)+str(j-1 if j>5 else j)]
-                    #         subject = cell_value[0].split('(')[0];
-                    #         slot_fillup.add( (subject, mapped_slot, cell_value[k]) )
-        # print(sub_vs_room)
-        # print(sub_vs_slot)
         sub_to_slot_name = {}
         for subject in sub_vs_slot:
             max_match = 0
@@ -102,12 +79,11 @@ def map_sheets(valid_sheets, wb):
                 if(present_match > max_match):
                     sub_to_slot_name[subject] = slot_name
                     max_match = present_match
-        # print(sub_to_slot_name)
+
         for subject in sub_to_slot_name:
             if('(' in subject or not sub_vs_room[subject]):
                 continue
             for room in sub_vs_room[subject]:
-                # print(subject, sub_to_slot_name[subject])
                 slot_fillup.add( (subject, sub_to_slot_name[subject], room) )
 
     return list(slot_fillup)
