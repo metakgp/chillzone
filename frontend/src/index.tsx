2aa2e0270c9f2911ea55e3563a8609d79820a809
Lakshya Satpal <lakshysatpal372@gmail.com>
2023-11-27 03:37:23 +0530

    added createRoot for react v5

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,15 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
+
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import schedule from "./data/schedule.json";
 import emptySchedule from "./data/empty_schedule.json";
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  rootElement.className = "container-fluid";
-}
+const rootElement = document.getElementById("root")!;
+rootElement.className = "container-fluid";
 
-ReactDOM.render(
-  <App schedule={schedule} emptySchedule={emptySchedule} />,
-  document.getElementById("root")
-);
+const root = createRoot(rootElement);
+root.render(<App schedule={schedule} emptySchedule={emptySchedule} />);
 
 registerServiceWorker();
