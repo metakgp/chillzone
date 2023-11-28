e2ea49e5a28cbcdf9e0769d740dcc184081692a3
Swarnendu Bhandari <94161758+Swarnendu0123@users.noreply.github.com>
2023-11-28 12:45:47 +0530

    css file added

---
+++ frontend/src/LearnMoreContent.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./LearnMoreContent.css";
+
+const LearnMoreContent = () => {
+  return (
+    <div>
+      <h3>Understanding Classroom Availability: Rules and Guidelines</h3>
+      <div className="learn-more-content">
+        <p>
+          1. Timetable Accuracy: Empty classrooms are based on the official
+          timetable provided by the institution's ERP system. Professors might
+          occasionally change classrooms without updating the ERP, leading to
+          discrepancies between the listed and actual locations.
+        </p>
+        <p>
+          2. Guard Access: Even if a classroom is empty according to the
+          timetable, it might not always be accessible. Guards have the
+          discretion to unlock or keep classrooms locked based on various
+          factors, ensuring security and maintenance.
+        </p>
+        <p>
+          3. Maintenance and Unforeseen Circumstances: Classrooms can be
+          unavailable due to maintenance, unexpected events, or other unforeseen
+          circumstances. Such situations might not be immediately updated in the
+          system, leading to discrepancies.
+        </p>
+        <p>
+          4. Real-Time Updates: The system might not always reflect real-time
+          changes. It's advisable to confirm the availability with the
+          institution's staff if you need a classroom urgently.
+        </p>
+        <p>
+          5. Room Reservation Policies: Some classrooms might be reserved for
+          specific purposes, such as meetings, events, or exams, even if they
+          appear empty in the timetable. It's important to adhere to the
+          institution's room reservation policies when planning to use a
+          classroom.
+        </p>
+        <p>
+          6. Flexibility and Communication: Flexibility is key when planning to
+          use a classroom. Always have a backup location in mind in case your
+          preferred classroom is unavailable. Communication with relevant
+          authorities, such as administrative staff or professors, can help in
+          securing a suitable classroom for your needs.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LearnMoreContent;
--- frontend/src/TwoSlotDisplay.js
+++ frontend/src/TwoSlotDisplay.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import EmptyRooms from './EmptyRooms.js';
-import { HourSlotMap, Slots, DayNames, Complexes, Floors } from './Constants.js';
-import { getNextSlot } from './Utilities.js';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import EmptyRooms from "./EmptyRooms.js";
+import {
+  HourSlotMap,
+  Slots,
+  DayNames,
+  Complexes,
+  Floors,
+} from "./Constants.js";
+import { getNextSlot } from "./Utilities.js";
+import LearnMoreContent from "./LearnMoreContent.js";
 
 class TwoSlotDisplay extends Component {
   constructor(props) {
-    super(props)
-    let slot = 0, today = props.date, complex = "Any", floor = "Any";
+    super(props);
+    let slot = 0,
+      today = props.date,
+      complex = "Any",
+      floor = "Any";
 
     let day = today.getDay() - 1;
     let isWeekend = day < 0 || day >= 5;
@@ -23,96 +33,145 @@ class TwoSlotDisplay extends Component {
     }
 
     this.state = this.Build(day, slot, complex, floor, isWeekend);
+    this.state = {
+      showLearnMoreContent: false,
+    };
   }
 
   Build(day, slot, complex, floor, isWeekend) {
-    return { day, slot, complex, floor, isWeekend }
+    return { day, slot, complex, floor, isWeekend };
   }
 
   static propTypes = {
     schedule: PropTypes.array.isRequired,
-    date: PropTypes.object.isRequired
-  }
+    date: PropTypes.object.isRequired,
+  };
 
   render() {
     let next = getNextSlot(this.state.day, this.state.slot);
     return (
       <div className="container">
-        <h3>
-          Choose the slot, area and floor that you want to chill at:
-        </h3>
-        { this.state.isWeekend && <h5>PS: You shouldn't be attending classes on weekends! :P</h5> }
+        <h3>Choose the slot, area and floor that you want to chill at:</h3>
+        <p>
+          The empty classes listed below might not be accurate in real life.
+          <a onClick={() => this.setState({ showLearnMoreContent: !this.state.showLearnMoreContent })}>
+            Learn More
+          </a>
+        </p>
+        {this.state.showLearnMoreContent && <LearnMoreContent />}
+
+        {this.state.isWeekend && (
+          <h5>PS: You shouldn't be attending classes on weekends! :P</h5>
+        )}
         <div className="row">
           <div className="col-md-12">
-            <select value={this.state.day} onChange={(event) => {
-              let newDay = parseInt(event.target.value, 10)
-              this.setState(this.Build(newDay, this.state.slot, this.state.complex, this.state.floor, this.state.isWeekend));
-            }}>
+            <select
+              value={this.state.day}
+              onChange={(event) => {
+                let newDay = parseInt(event.target.value, 10);
+                this.setState(
+                  this.Build(
+                    newDay,
+                    this.state.slot,
+                    this.state.complex,
+                    this.state.floor,
+                    this.state.isWeekend
+                  )
+                );
+              }}
+            >
               {DayNames.map((val, ind) => (
-                <option value={ind}>
-                  {val}
-                </option>
+                <option value={ind}>{val}</option>
               ))}
             </select>
-            <select value={this.state.slot} onChange={(event) => {
-              let newSlot = parseInt(event.target.value, 10);
-              this.setState(this.Build(this.state.day, newSlot, this.state.complex, this.state.floor, this.state.isWeekend));
-            }}>
+            <select
+              value={this.state.slot}
+              onChange={(event) => {
+                let newSlot = parseInt(event.target.value, 10);
+                this.setState(
+                  this.Build(
+                    this.state.day,
+                    newSlot,
+                    this.state.complex,
+                    this.state.floor,
+                    this.state.isWeekend
+                  )
+                );
+              }}
+            >
               {Slots.map((val, ind) => (
-                <option value={ind}>
-                  {val}
-                </option>
+                <option value={ind}>{val}</option>
               ))}
             </select>
-            <select value={this.state.complex} onChange={(event) => {
-              let newComplex = event.target.value
-              this.setState(this.Build(this.state.day, this.state.slot, newComplex, this.state.floor, this.state.isWeekend))
-            }}>
-              {Object.keys(Complexes).map((val)=>(
-                <option value={val}>
-                  {val}
-                </option>
+            <select
+              value={this.state.complex}
+              onChange={(event) => {
+                let newComplex = event.target.value;
+                this.setState(
+                  this.Build(
+                    this.state.day,
+                    this.state.slot,
+                    newComplex,
+                    this.state.floor,
+                    this.state.isWeekend
+                  )
+                );
+              }}
+            >
+              {Object.keys(Complexes).map((val) => (
+                <option value={val}>{val}</option>
               ))}
             </select>
-            <select value={this.state.floor} onChange={(event) => {
-              let newFloor = event.target.value
-              this.setState(this.Build(this.state.day, this.state.slot, this.state.complex, newFloor, this.state.isWeekend))
-            }}>
-              {Object.keys(Floors).map((val)=>(
-                <option value={val}>
-                  {val}
-                </option>
+            <select
+              value={this.state.floor}
+              onChange={(event) => {
+                let newFloor = event.target.value;
+                this.setState(
+                  this.Build(
+                    this.state.day,
+                    this.state.slot,
+                    this.state.complex,
+                    newFloor,
+                    this.state.isWeekend
+                  )
+                );
+              }}
+            >
+              {Object.keys(Floors).map((val) => (
+                <option value={val}>{val}</option>
               ))}
             </select>
           </div>
         </div>
         <div className="row">
           <div className="col-md-12">
-            <i>
-              Rooms in BOLD are free for the next slot as well
-            </i>
+            <i>Rooms in BOLD are free for the next slot as well</i>
           </div>
         </div>
         <div className="row">
           <div className="col-md-6">
-            <EmptyRooms schedule={this.props.schedule}
-                        day={this.state.day}
-                        slot={this.state.slot}
-                        complex={this.state.complex}
-                        floor={this.state.floor}
-                        show_common_next={true} />
+            <EmptyRooms
+              schedule={this.props.schedule}
+              day={this.state.day}
+              slot={this.state.slot}
+              complex={this.state.complex}
+              floor={this.state.floor}
+              show_common_next={true}
+            />
           </div>
           <div className="col-md-6">
-            <EmptyRooms schedule={this.props.schedule}
-                        day={next.day}
-                        slot={next.slot}
-                        complex={this.state.complex}
-                        floor={this.state.floor}
-                        show_common_next={true} />
+            <EmptyRooms
+              schedule={this.props.schedule}
+              day={next.day}
+              slot={next.slot}
+              complex={this.state.complex}
+              floor={this.state.floor}
+              show_common_next={true}
+            />
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
