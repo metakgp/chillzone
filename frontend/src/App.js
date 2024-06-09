734adbb5ae6b0f7da1ba02061307aa9b2817e228
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-09 20:28:12 +0530

    refactor: remove unwanted imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import CustomTable from "./CustomTable.js";
 import PropTypes from "prop-types";
